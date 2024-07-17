12e3c7ef106833601932143972edde146d479aad
dalglishybosco <dalglish.fernandes@sky.uk>
2024-07-17 15:07:54 +0100

    entrypoint updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
-# Inspired by concourse/docker-image-resource:
-# https://github.com/concourse/docker-image-resource/blob/master/assets/common.sh
-
 set -o errexit -o pipefail -o nounset
 
-# Waits DOCKERD_TIMEOUT seconds for startup (default: 60)
 DOCKERD_TIMEOUT="${DOCKERD_TIMEOUT:-60}"
-# Accepts optional DOCKER_OPTS (default: --data-root /scratch/docker)
 DOCKER_OPTS="${DOCKER_OPTS:-}"
 
-# Constants
 DOCKERD_PID_FILE="/tmp/docker.pid"
 DOCKERD_LOG_FILE="/tmp/docker.log"
 
@@ -26,11 +20,8 @@ sanitize_cgroups() {
   fi
   mount -o remount,rw "${cgroup}"
 
-  # Skip AppArmor
-  # See: https://github.com/moby/moby/commit/de191e86321f7d3136ff42ff75826b8107399497
   export container=docker
 
-  # Mount /sys/kernel/security
   if [[ -d /sys/kernel/security ]] && ! mountpoint -q /sys/kernel/security; then
     if ! mount -t securityfs none /sys/kernel/security; then
       echo >&2 "Could not mount /sys/kernel/security."
@@ -40,13 +31,11 @@ sanitize_cgroups() {
 
   sed -e 1d /proc/cgroups | while read -r sys hierarchy num enabled; do
     if [[ "${enabled}" != "1" ]]; then
-      # subsystem disabled; skip
       continue
     fi
 
     grouping="$(cat /proc/self/cgroup | cut -d: -f2 | grep "\\<${sys}\\>")"
     if [[ -z "${grouping}" ]]; then
-      # subsystem not mounted anywhere; mount it on its own
       grouping="${sys}"
     fi
 
@@ -54,7 +43,6 @@ sanitize_cgroups() {
 
     mkdir -p "${mountpoint}"
 
-    # clear out existing mount to make sure new one is read-write
     if mountpoint -q "${mountpoint}"; then
       umount "${mountpoint}"
     fi
@@ -70,15 +58,12 @@ sanitize_cgroups() {
     fi
   done
 
-  # Initialize systemd cgroup if host isn't using systemd.
-  # Workaround for https://github.com/docker/for-linux/issues/219
   if ! [[ -d /sys/fs/cgroup/systemd ]]; then
     mkdir "${cgroup}/systemd"
     mount -t cgroup -o none,name=systemd cgroup "${cgroup}/systemd"
   fi
 }
 
-# Setup container environment and start docker daemon in the background.
 start_docker() {
   echo >&2 "Setting up Docker environment..."
   mkdir -p /var/log
@@ -86,20 +71,17 @@ start_docker() {
 
   sanitize_cgroups
 
-  # check for /proc/sys being mounted readonly, as systemd does
   if grep '/proc/sys\s\+\w\+\s\+ro,' /proc/mounts >/dev/null; then
     mount -o remount,rw /proc/sys
   fi
 
   local docker_opts="${DOCKER_OPTS:-}"
 
-  # Pass through `--garden-mtu` from gardian container
   if [[ "${docker_opts}" != *'--mtu'* ]]; then
     local mtu="$(cat /sys/class/net/$(ip route get 8.8.8.8 | awk '{ print $5 }')/mtu)"
     docker_opts+=" --mtu ${mtu}"
   fi
 
-  # Use Concourse's scratch volume to bypass the graph filesystem by default
   if [[ "${docker_opts}" != *'--data-root'* ]] && [[ "${docker_opts}" != *'--graph'* ]]; then
     docker_opts+=' --data-root /scratch/docker'
   fi
@@ -112,8 +94,6 @@ start_docker() {
   echo "$!" > "${DOCKERD_PID_FILE}"
 }
 
-# Wait for docker daemon to be healthy
-# Timeout after DOCKERD_TIMEOUT seconds
 await_docker() {
   local timeout="${DOCKERD_TIMEOUT}"
   echo >&2 "Waiting ${timeout} seconds for Docker to be available..."
@@ -142,10 +122,23 @@ await_docker() {
   echo >&2 "Docker available after ${duration} seconds."
 }
 
-# Gracefully stop Docker daemon.
 stop_docker() {
   if ! [[ -f "${DOCKERD_PID_FILE}" ]]; then
     return 0
   fi
   local docker_pid
-  docker_pid="$(cat ${DO
+  docker_pid="$(cat ${DOCKERD_PID_FILE})"
+  if kill -0 "${docker_pid}"; then
+    echo >&2 "Stopping Docker daemon..."
+    kill "${docker_pid}"
+    wait "${docker_pid}"
+  fi
+  rm -f "${DOCKERD_PID_FILE}"
+}
+
+trap stop_docker EXIT
+
+start_docker
+await_docker
+
+exec "$@"
